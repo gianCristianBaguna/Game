4634c4a8a2ae70d090dae65ea273ec5d52fa4d8a
Gian <102247108+gianCristianBaguna@users.noreply.github.com>
2023-05-16 12:25:26 +0800

    Update duel.ts

--- duel.ts
+++ duel.ts
@@ -1,4 +1,4 @@
-class Hero{
+class Hero {
 	name: string;
 	sprite: string;
 	hp: number;
@@ -14,7 +14,7 @@ class Hero{
 	}
 }
 
-let mememons: Mememon[] = [
+let heroes: Hero[] = [
 	[
 		"Goku",
 		"assets/dragonBalls/goku.png",
@@ -44,3 +44,37 @@ let mememons: Mememon[] = [
 	],
 ].map(([name, sprite, hp]) => new Mememon(name, sprite, hp, moves));
 
+let typeMatch: { [key: string]: string[][] } = {
+	DaWae: [["cringe"], ["deepfried", "rock"], ["dank", "wholesome", "classic"]],
+	BigChungus: [[""], ["wholesome"], ["dank", "deepfried"]],
+	Spongeken: [
+		["poison"],
+		["dank", "rage", "dark", "classic"],
+		["wholesome", "deepfried"],
+	],
+};
+
+function spawn(bool: boolean): Mememon {
+	let p = mememons[Math.floor(Math.random() * mememons.length)];
+	let mmm = new Mememon(p.name, p.sprite, p.hp, p.moves);
+
+	if (bool) {
+		for (let i = 0; i < 4; i++) {
+			let move = mmm.moves[i];
+			document.getElementById("m" + i).value = move[0];
+		}
+	}
+	return mmm;
+}
+
+let mm1 = spawn(true);
+let s1 = document.createElement("img");
+s1.src = mm1.sprite;
+document.getElementById("mm1").appendChild(s1);
+document.getElementById("hp1").innerHTML = `<h1>Player</h1><br><p>HP: ${mm1.hp}/${mm1.fullhp}</p>`;
+
+let mm2 = spawn(false);
+let s2 = document.createElement("img");
+s2.src = mm2.sprite;
+document.getElementById("mm2").appendChild(s2);
+document.getElementById("hp2
