012248b1aa5d34ee330c1aadaf8a614f136722f1
Gian <102247108+gianCristianBaguna@users.noreply.github.com>
2023-05-23 20:52:41 +0800

    deleted duel.html

--- characterPicking.js
+++ characterPicking.js
@@ -17,6 +17,12 @@ gohan.addEventListener('click', function () {
 trunks.addEventListener('click', function () {
     window.location.href = 'trunks.html';
 });
+android18.addEventListener('click', function () {
+    window.location.href = 'android18.html';
+});
+krilin.addEventListener('click', function () {
+    window.location.href = 'krilin.html';
+});
 captainAmerica.addEventListener('click', function () {
     window.location.href = 'captainAmerica.html';
 });
@@ -25,4 +31,10 @@ ironman.addEventListener('click', function () {
 });
 thor.addEventListener('click', function () {
     window.location.href = 'thor.html';
+});
+hulk.addEventListener('click', function () {
+    window.location.href = 'hulk.html';
+});
+spiderman.addEventListener('click', function () {
+    window.location.href = 'spiderman.html';
 });
