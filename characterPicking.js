1068b666f9fccc7bb06e5a4672e3df9eda6ef613
Gian <102247108+gianCristianBaguna@users.noreply.github.com>
2023-05-23 19:59:43 +0800

    changed the picking of character

---
+++ characterPicking.js
@@ -0,0 +1,40 @@
+var goku = document.getElementById('goku');
+var gohan = document.getElementById('gohan');
+var trunks = document.getElementById('trunks');
+var android18 = document.getElementById('android18');
+var krilin = document.getElementById('krilin');
+var captainAmerica = document.getElementById('captainAmerica');
+var ironman = document.getElementById('ironman');
+var thor = document.getElementById('thor');
+var hulk = document.getElementById('hulk');
+var spiderman = document.getElementById('spiderman');
+goku.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+gohan.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+trunks.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+android18.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+krilin.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+captainAmerica.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+ironman.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+thor.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+hulk.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
+spiderman.addEventListener('click', function () {
+    window.location.href = 'game.html';
+});
---
+++ characterPicking.ts
@@ -0,0 +1,51 @@
+const goku = document.getElementById('goku');
+const gohan = document.getElementById('gohan');
+const trunks = document.getElementById('trunks');
+const android18 = document.getElementById('android18');
+const krilin = document.getElementById('krilin');
+const captainAmerica = document.getElementById('captainAmerica');
+const ironman = document.getElementById('ironman');
+const thor = document.getElementById('thor');
+const hulk = document.getElementById('hulk');
+const spiderman = document.getElementById('spiderman');
+
+goku.addEventListener('click', function() {
+  window.location.href = 'game.html';
+});
+
+gohan.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+
+trunks.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+
+android18.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+  
+krilin.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+
+captainAmerica.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+
+ironman.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+
+thor.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+
+hulk.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+
+spiderman.addEventListener('click', function() {
+    window.location.href = 'game.html';
+  });
+  
