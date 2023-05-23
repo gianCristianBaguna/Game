baec01626beb9e1d83209160c7dd8a22cbbedc81
Gian <102247108+gianCristianBaguna@users.noreply.github.com>
2023-05-23 20:43:58 +0800

    Update characterPicking.ts

--- characterPicking.ts
+++ characterPicking.ts
@@ -21,6 +21,14 @@ trunks.addEventListener('click', function() {
     window.location.href = 'trunks.html';
   });
 
+android18.addEventListener('click', function() {
+    window.location.href = 'android18.html';
+  });
+  
+krilin.addEventListener('click', function() {
+    window.location.href = 'krilin.html';
+  });
+
 captainAmerica.addEventListener('click', function() {
     window.location.href = 'captainAmerica.html';
   });
@@ -32,3 +40,12 @@ ironman.addEventListener('click', function() {
 thor.addEventListener('click', function() {
     window.location.href = 'thor.html';
   });
+
+hulk.addEventListener('click', function() {
+    window.location.href = 'hulk.html';
+  });
+
+spiderman.addEventListener('click', function() {
+    window.location.href = 'spiderman.html';
+  });
+  
