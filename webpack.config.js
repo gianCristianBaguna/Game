70130b5f5b9bb7af3e8c6a80f039f10c79fb7fcd
Gian <102247108+gianCristianBaguna@users.noreply.github.com>
2023-05-30 00:52:51 +0800

    added styles

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './game-logics.ts', // Replace with the entry file of your TypeScript code
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+};
